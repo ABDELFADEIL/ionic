e81ab7a0126e82cf80b836b135cf215aef5468a4
ABDELFADEIL <abdalfadeil@gmail.com>
2020-11-23 21:50:35 +0100

    demo_etudents

--- demo_students/src/app/home/home.page.ts
+++ demo_students/src/app/home/home.page.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {StudentService} from "../services/student.service";
+import {Student} from "../models/student";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  message: string ="";
+  students: Student [] = [];
+  private student: Student;
+  constructor(private router: Router, private studentService: StudentService) {
+  }
 
-  constructor() {}
+  ngOnInit(): void {
+    this.findAllStudents();
+    this.countAlternant();
+  }
+  findAllStudents(){
+    this.studentService.findAll().subscribe(data => {
+       this.students = data;
+      this.countAlternant();
+    });
+  }
+  findStudentById(id:number){
+    this.studentService.findById(id).subscribe(data => {
+      this.student = data;
+    })
+  }
+
+  private countAlternant() {
+    let numAlternant = 0;
+    this.students.forEach(student => {
+      if (student.alternant)
+      numAlternant++;
+    })
+    this.message = "Nombre d'étudiants en alternant: "+numAlternant;
+    return this.message;
+  }
+
+  onChange(id:number){
+    for (let i=0; i < this.students.length; i++){
+      console.log(id, i)
+      if (id == this.students[i].id){
+        this.students[i].alternant = !this.students[i].alternant;
+        this.onUpdateStudent(this.students[i]);
+        break;
+      }
+
+    }
+    this.countAlternant();
+  }
+
+  onUpdateStudent(student:Student){
+    this.studentService.update(student).subscribe(data => {
+      console.log(data);
+    })
+  }
+  etudentDetails(id:number){
+    this.router.navigateByUrl('/student/'+id)
+  }
 
 }
---
+++ demo_students/src/app/models/student.ts
@@ -0,0 +1,9 @@
+export class Student {
+    id:number;
+    name:string;
+    lastname: string;
+    average:number;
+    alternant: boolean;
+    age: number
+
+}
